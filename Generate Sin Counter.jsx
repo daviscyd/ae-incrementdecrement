aae3d4f5fdfc7bf894e7e6cea589093f29b46946
daviscyd <57862285+daviscyd@users.noreply.github.com>
2019-11-17 05:44:45 -0600

    Add files via upload
    Initial repo seeding

---
+++ Generate Sin Counter.jsx
@@ -0,0 +1,21 @@
+﻿var oLayers = app.project.activeItem.layers;
+var oIC = app.project.items;
+
+var oAVLayers = [];
+for (var i = 1; i <= oLayers.length; i++){
+    if (oLayers[i].hasAudio == true && oLayers[i].hasVideo == false){
+    oAVLayers.push(oLayers[i]);
+        }else if (oLayers[i].matchName == "ADBE Text Layer"){
+            var oTxt = oLayers[1];
+        }
+    }
+
+oTxt.property("Source Text").setValueAtTime(0, 0);
+
+for (var i = 0; i < oAVLayers.length; i++){
+    if (oAVLayers[i].name == "Gnid"){
+         oTxt.property("Source Text").setValueAtTime(oAVLayers[i].marker.keyTime(1), (new TextDocument(i-1)));}
+     else{
+    oTxt.property("Source Text").setValueAtTime(oAVLayers[i].marker.keyTime(1), (new TextDocument(i + 1)));}
+     }
+$.writeln(oTxt.property("Source Text").numKeys);
