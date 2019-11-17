a12c97455f48367fa75e2446a0a9581465d24c51
daviscyd <57862285+daviscyd@users.noreply.github.com>
2019-11-17 07:13:17 -0600

    Update Generate Counter.jsx
    Corrected file

--- Generate Counter.jsx
+++ Generate Counter.jsx
@@ -1,21 +1,41 @@
-﻿var oLayers = app.project.activeItem.layers;
+// required to use forEach
+#include "C:\\Program Files\\Adobe\\Adobe After Effects 2020\\Support Files\\Scripts\\es5-shim.jsx"
+
+var oLayers = app.project.activeItem.layers;
 var oIC = app.project.items;
+
+var oTL = oLayers.addText("");
+var oTP = oTL.property("Source Text");
+var oTD = oTP.value;
+
+oTD.font = "MyriadPro-Cond";
+oTD.fontSize = 24;
 
 var oAVLayers = [];
 for (var i = 1; i <= oLayers.length; i++){
     if (oLayers[i].hasAudio == true && oLayers[i].hasVideo == false){
     oAVLayers.push(oLayers[i]);
-        }else if (oLayers[i].matchName == "ADBE Text Layer"){
-            var oTxt = oLayers[1];
+        }else{
+            continue;
         }
     }
 
-oTxt.property("Source Text").setValueAtTime(0, 0);
+oTL.property("Position").expression = "transform.position = [10, 48];"
 
-for (var i = 0; i < oAVLayers.length; i++){
-    if (oAVLayers[i].name == "Gnid"){
-         oTxt.property("Source Text").setValueAtTime(oAVLayers[i].marker.keyTime(1), (new TextDocument(i-1)));}
-     else{
-    oTxt.property("Source Text").setValueAtTime(oAVLayers[i].marker.keyTime(1), (new TextDocument(i + 1)));}
-     }
-$.writeln(oTxt.property("Source Text").numKeys);
+// initial counter value
+oTP.setValueAtTime(0, 0);
+
+// initialize counter
+var i = 0;
+
+// start forEach
+ oAVLayers.forEach(myFunction)
+// increment/decrement heavy lifting done here
+function myFunction(item, index, arr) {
+if (arr[index].name == "ShinyDing.aiff")
+  {   i++
+      oTL.property("Source Text").setValueAtTime(arr[index].marker.keyTime(1), (new TextDocument(i)));}
+else if (arr[index].name == "ShinyGnid.aiff")
+	{  i--
+        oTL.property("Source Text").setValueAtTime(arr[index].marker.keyTime(1), (new TextDocument(i)));}
+}
